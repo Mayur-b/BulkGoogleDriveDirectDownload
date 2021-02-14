0aa3fddc389ef3ba028896cbd1d72ad974127f8c
Mayur Bhatolia <mayur.bhatolia@gmail.com>
2021-02-14 16:46:04 +0530

    Update directDownload.js

--- directDownload.js
+++ directDownload.js
@@ -2,21 +2,21 @@ function myFunction() {
   var ss=SpreadsheetApp.getActiveSpreadsheet();
   var s=ss.getActiveSheet();
   var c=s.getActiveCell();
-  var fldr=DriveApp.getFolderById("<id>");
+  var fldr=DriveApp.getFolderById("18SXHIXtGk_Clm0pxtOpPRdW-BpuR740_");
   var files=fldr.getFiles();
   
   var names=[], links=[];
-  var f,firstName, name, strLink, ;
+  var f, name, directURL;
   
   while (files.hasNext()) {
     f=files.next();
-    directURL = f.getUrl().replace(/\/file\/d\/(.+)\/(.+)/, "/uc?export=download&id=$1");
-    name = f.getName().split(/(\s+)/);  //I had to use only the first part of the name hence I used name[0]. You could directly use f.getName();
-    firstName='concat("","' + name[0] + '")';
-    strLink= 'concat("","' + directURL + '")';
     
-    names.push([firstName]);
-    links.push([strLink]);
+    directURL = f.getUrl().replace(/\/file\/d\/(.+)\/(.+)/, "/uc?export=download&id=$1");
+    name = f.getName().split(/(\s+)/);  
+    
+    //I had to use only the first part of the name hence I used name[0]. You could directly use f.getName() for the full file name;
+    names.push(['concat("","' + name[0] + '")']);
+    links.push(['concat("","' + directURL + '")']);
   }
   
   s.getRange(c.getRow(),c.getColumn(),names.length).setFormulas(names);
